20ba5efe7f18311078ef636f9004aed19c817a6d
Анджей Щетинин <tetragrammaton54@mail.ru>
2022-07-23 04:48:01 +0700

    Add project files.

---
+++ PozorWithBlazor/Server/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PozorWithBlazor.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string userName, string message)
+            => await Clients.All.SendAsync("ReceiveMessage", userName, message);
+    }
+}
---
+++ PozorWithBlazor/Server/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.ResponseCompression;
+using PozorWithBlazor.Server.Hubs;
+
+namespace PozorWithBlazor.Server
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddSignalR();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseResponseCompression();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+                endpoints.MapFallbackToFile("index.html");
+                endpoints.MapHub<ChatHub>("/chathub");
+            });
+        }
+
+    }
+}
